5e81b3e4af67ac77dfa12cd4a34dbd9b1e3bdf6b
Tejvir007 <97439718+Tejvir007@users.noreply.github.com>
2022-01-14 19:53:59 +0530

    Add files via upload

---
+++ Asg1_CSE_21103088_output.py
@@ -0,0 +1,30 @@
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+Install the latest PowerShell for new features and improvements! https://aka.ms/PSWindows
+
+PS E:\cprgms> & C:/Users/DELLS/AppData/Local/Programs/Python/Python310/python.exe e:/cprgms/Asg1_CSE_21103088_input.py
+Enter the value of a:10
+Enter the value of b:12
+Enter the value of c:14
+The average of the given numbers =  12
+Please enter your gross income to the nearest penny 100000
+Please enter the number of dependents 4
+Total Payable Tax =  15600.0
+Please enter your Student ID: 21103088
+Please enter your name: Tejvir
+Gender : M/F/U M
+Please enter your branch name: CSE
+Please enter your cgpa 10
+['21103088', 'Tejvir', 'M', 'CSE', '10']
+Student 1: 90
+Student 2: 95
+Student 3: 80
+Student 4: 86
+Student 5: 89
+List of marks of the students:  [90, 95, 80, 86, 89]
+List of marks of the students in the ascending order:  [80, 86, 89, 90, 95]
+List of marks of the students in the descenind order:  [95, 90, 89, 86, 80]
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'purple', 'Yellow']
+PS E:\cprgms> 
