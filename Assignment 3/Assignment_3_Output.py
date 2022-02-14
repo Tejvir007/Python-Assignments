c4f2f095ea59cb87504012f68d76c3f1d2348273
Tejvir007 <97439718+Tejvir007@users.noreply.github.com>
2022-02-14 19:19:16 +0530

    Add files via upload

---
+++ Assignment 3/Assignment_3_Output.py
@@ -0,0 +1,83 @@
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+Install the latest PowerShell for new features and improvements! https://aka.ms/PSWindows
+
+PS E:\Assignments> & C:/Users/DELLS/AppData/Local/Programs/Python/Python310/python.exe e:/Assignments/assignment_3.py
+Question 1 
+
+Enter the string : My name is Tejvir
+{'My': 1, 'name': 1, 'is': 1, 'Tejvir': 1}
+Question 2
+
+Enter the month : 2
+Enter the day : 28
+Enter the year : 2004
+Date entered is 28/2/2004
+Next Date is : 29/2/2004
+Question 3
+
+Enter the number of elements : 4
+Enter elements 
+1
+2
+3
+4
+Entered list is :  [1, 2, 3, 4]
+List of tuples :
+[(1, 1), (2, 4), (3, 9), (4, 16)]
+Question 4
+
+Enter the grade points10
+Your Grade is A+ and Outstanding Performance
+Question 5
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+Question 6
+
+Enter the name of the student : Tejvir
+Enter the SID of the student : 21103088
+Enter Y to add details of more students otherwise enter N Y
+Enter the name of the student : Ankit
+Enter the SID of the student : 21103050
+Enter Y to add details of more students otherwise enter N Y
+Enter the name of the student : Deepank
+Enter the SID of the student : 21103075
+Enter Y to add details of more students otherwise enter N Y
+Enter the name of the student : Manik
+Enter the SID of the student : 21103108
+Enter Y to add details of more students otherwise enter N N
+{'21103088': 'Tejvir', '21103050': 'Ankit', '21103075': 'Deepank', '21103108': 'Manik'}
+Dictinoary after sorting by Student Name :
+{'21103050': 'Ankit', '21103075': 'Deepank', '21103108': 'Manik', '21103088': 'Tejvir'}
+Dictionary after sorting by SID
+{'21103050': 'Ankit', '21103075': 'Deepank', '21103088': 'Tejvir', '21103108': 'Manik'}
+Enter SID to find student name : 21103088
+Name of the Student :  Tejvir
+Question 7
+
+Enter number of terms of fibonacci series to print10
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+Average of series:  8.8
+Question 8
+
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
+PS E:\Assignments> 
